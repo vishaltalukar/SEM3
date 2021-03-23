4c90805a608ee448cf84f7cd6498202f109f2c28
Vishal Talukar <64702006+vishaltalukar@users.noreply.github.com>
2021-03-23 23:37:22 +0530

    Add files via upload

---
+++ eval.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class eval {
+   public static void checkage(int age){
+        if(age<18){
+            System.out.println("Not Eligible to VOTE");
+        }
+        else {
+            System.out.println("Eligible to VOTE");
+        }
+    }
+}
